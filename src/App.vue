4d14ee5a2e5b0200d40b55a7945dfdc9b33631bb
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-06-09 01:23:49 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="flex gap-4">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <h1 class="text-8xl font-bold text-sky-400">Hello World Starter ! 🛫</h1>
+
+  <RouterView />
+</template>
